41c7eb90f96cef8e55efc1abb73cb16be97ad63e
gzapas <gzapas@WND10DEV>
2016-07-10 20:59:21 +0300

    Add project files.

---
+++ WpfApplicationTestMvvmOne/Models/Customer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplicationTestMvvmOne.Models
+{
+    public class Customer
+    {
+
+        #region Fields
+
+        private Double _amount;
+        private String _customerName;
+        private String _isMarried;
+        private Double _tax;
+        #endregion
+
+
+        public Double Amount { get { return _amount; } set { _amount = value; } }
+        public String CustomerName { get { return _customerName; } set { _customerName = value; } }
+        public String IsMarried { get { return _isMarried; } set { _isMarried = value; } }
+        public Double Tax { get { return _tax; } set { _tax = value; } }
+
+        #region Methods
+
+        public void CalculateTax()
+        {
+            if (_amount > 2000)
+            {
+                _tax = 20;
+            }
+            else if (_amount > 1000)
+            {
+                _tax = 10;
+            }
+            else { _tax = 5; }
+        }
+
+        #endregion
+    }
+}
---
+++ WpfApplicationTestMvvmOne/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApplicationTestMvvmOne.Models;
+
+namespace WpfApplicationTestMvvmOne.ViewModels
+{
+    public class CustomerViewModel
+    {
+        private Customer cust = new Customer();
+
+        public String TxtCustomerName
+        {
+            get { return cust.CustomerName; }
+            set { cust.CustomerName = value; }
+        }
+
+        public String TxtAmount
+        {
+            get { return Convert.ToString(cust.Amount); }
+            set { cust.Amount = Convert.ToDouble(value); }
+        }
+
+        public String LblAmountColor
+        {
+            get
+            {
+                if (cust.Amount > 2000)
+                {
+                    return "Blue";
+                }
+                else if(cust.Amount > 1500)
+                {
+                    return "Red";
+                }
+                return "Yellow";
+            }
+        }
+
+        public Boolean IsMarried
+        {
+            get
+            {
+                if (cust.IsMarried == "Married") { return true; }
+                else
+                {
+                    return false;
+                }
+            }
+            set {
+                if (value == true) { cust.IsMarried = "Married"; }
+                else { cust.IsMarried = "Not Married"; }
+            }
+        }
+
+        public void CalculateTax() { cust.CalculateTax(); }
+    }
+}
