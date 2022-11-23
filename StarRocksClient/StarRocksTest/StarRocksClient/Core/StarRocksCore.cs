5c3f20829f724821f34483ec9859f732e839068b
Grigoris Zapantis <grigoris.zapantis@hellasdirect.gr>
2022-11-23 23:43:11 +0200

    added starrocks client

---
+++ StarRocksClient/StarRocksTest/StarRocksClient/Core/StarRocksCore.cs
@@ -0,0 +1,59 @@
+﻿using Dapper;
+using MySql.Data.MySqlClient;
+using StarRocksClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.InteropServices.ObjectiveC;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarRocksClient.Core
+{
+    public class StarRocksCore
+    {
+        #region Fields
+        private readonly string _CONN_STR_ = "Server=192.168.203.8;Database=testdb;Port=9030;Uid=root;Pwd=tzimakos;";
+        private static IDbConnection _conn;
+        #endregion
+
+        public StarRocksCore()
+        {
+            _conn = new MySqlConnection(_CONN_STR_);
+        }
+
+        public void Start() 
+        {
+            int id = 3;
+            var testitem = GetTestItemById(id);
+            if (testitem == null)
+            { 
+                Console.WriteLine($"No Item with id: {id} exists");
+                testitem = new TestItem(id);
+                var res = InsertItem(testitem);
+                Console.WriteLine($"Inserted Item with Id {id}. Result is {res}.");
+            }
+            else
+            {
+                Console.WriteLine($"Found Item with Id: {id} ");
+            }
+        }
+
+        private TestItem? GetTestItemById(int id) 
+        {
+            object parms = new { key = id };
+            var testitem = _conn.QueryFirstOrDefault<TestItem>("select * from testitem where Id = @key;",parms);
+            return testitem;
+        }
+
+        private int InsertItem(TestItem testItem) 
+        {
+            string sql = "INSERT INTO testitem (Id, Name, OrderDate, ItemPrice, ItemInStock, ItemWarehouse, ItemRevenue, ItemQuantity) " +
+                         $"select {testItem.Id}, '{testItem.Name}', '{testItem.OrderDate}', {testItem.ItemPrice}, {(testItem.ItemInStock?"1":"0")}, " +
+                         $"'{testItem.ItemWarehouse}', {testItem.ItemRevenue}, {testItem.ItemQuantity};";
+            var res = _conn.Execute(sql);
+            return res;
+        }
+    }
+}
---
+++ StarRocksClient/StarRocksTest/StarRocksClient/Models/TestItem.cs
@@ -0,0 +1,32 @@
+﻿namespace StarRocksClient.Models
+{
+    public class TestItem
+    {
+        #region Constructor
+        public TestItem(){}
+        public TestItem(int i)
+        {
+            Id = i++;
+            Name = $"ProductName{i}";
+            OrderDate = DateTime.Today.AddDays(i - (i * 3)).ToString("yyyy-MM-dd HH:mm:ss");
+            ItemPrice = (30 + i)/2;
+            ItemInStock = i % 2 == 0 ? true : false ;
+            ItemWarehouse = $"Warehouse - {i}";
+            ItemRevenue = ItemPrice * (10 / 100);
+            ItemQuantity = 2 + (i % 3);
+        }
+        #endregion
+
+        #region Properties
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string OrderDate { get; set; }
+
+        public double ItemPrice { get; set; }
+        public bool ItemInStock { get; set; }
+        public string ItemWarehouse { get; set; }
+        public double ItemRevenue { get; set; }
+        public int ItemQuantity { get; set; } 
+        #endregion
+    }
+}
